4d84747c9dabddd38c6848e60c402c2c57fb7b9d
Julien Reszka <contact.reszkajulien@gmail.com>
2016-12-21 20:12:25 +0100

    les bases du fillit sont la

--- jreszka/main.c
+++ jreszka/main.c
@@ -32,12 +32,15 @@ typedef struct 	s_tetrimino
 	t_point ref_point_max;
 	t_point hash[4];
 	char lettre;
+	int largeur;
+	int hauteur;
 }				t_tetrimino;
 
 typedef struct 	s_map
 {
 	int size;
 	char **content;
+	t_point iter_point;
 }				t_map;
 
 
@@ -225,37 +228,7 @@ int		main(int argc, char **argv)
 	}
 	*/
 
-/*
-**	Initialisation carre minimal
-*/
 
-	char **map;
-	map = (char**)malloc(sizeof(char*) * (min_square_space + 1));
-	int hauteur;
-	hauteur = 0;
-	int indexe;
-	while(hauteur < min_square_space)
-	{
-		map[hauteur] = (char*)malloc(sizeof(char) * (min_square_space + 1));
-		indexe = 0;
-		while (indexe < min_square_space)
-		{
-			map[hauteur][indexe] = '.';
-			indexe++;
-		}
-		map[hauteur][indexe] = '\0';
-		hauteur++;
-	}
-	map[hauteur] = 0;
-	/*
-
-	printf("\n");
-	while(*map)
-	{
-		printf("%s\n", *map);
-		map++;
-	}
-	*/
 
 /*
 **	Recuperation des coordonnees des # dans une structure de tetrimino
@@ -328,11 +301,17 @@ int		main(int argc, char **argv)
 		tetrimino[tetris_iter].ref_point_min.y = min_y;
 		tetrimino[tetris_iter].ref_point_max.x = max_x;
 		tetrimino[tetris_iter].ref_point_max.y = max_y;
-		printf("\nmin_x %d\nmin_y %d\nmax_x %d\nmax_y %d\n\n",
+		tetrimino[tetris_iter].largeur = max_x - min_x;
+		tetrimino[tetris_iter].hauteur = max_y - min_y;
+		printf("\nmin_x %d\nmin_y %d\nmax_x %d\nmax_y %d",
 			tetrimino[tetris_iter].ref_point_min.x,
 			tetrimino[tetris_iter].ref_point_min.y,
 			tetrimino[tetris_iter].ref_point_max.x,
 			tetrimino[tetris_iter].ref_point_max.y
+			);
+		printf("\nlargeur %d\nhauteur %d\n",
+			tetrimino[tetris_iter].largeur,
+			tetrimino[tetris_iter].hauteur
 			);
 		tetris_iter++;
 	}
@@ -346,7 +325,7 @@ int		main(int argc, char **argv)
 	while (tetris_iter < tetriminos_nb)
 	{
 		tetrimino[tetris_iter].lettre = charact + tetris_iter;
-		printf("%c\n", tetrimino[tetris_iter].lettre);
+		printf("\n%c\n", tetrimino[tetris_iter].lettre);
 		tetris_iter++;
 	}
 
@@ -358,72 +337,99 @@ int		main(int argc, char **argv)
 */
 
 
+/*
+**	Initialisation carre minimal
+*/
 
-
-
-
-
+	char **map;
+	map = (char**)malloc(sizeof(char*) * (min_square_space + 1));
+	int hauteur;
+	hauteur = 0;
+	int indexe;
+	while(hauteur < min_square_space)
+	{
+		map[hauteur] = (char*)malloc(sizeof(char) * (min_square_space + 1));
+		indexe = 0;
+		while (indexe < min_square_space)
+		{
+			map[hauteur][indexe] = '.';
+			indexe++;
+		}
+		map[hauteur][indexe] = '\0';
+		hauteur++;
+	}
+	map[hauteur] = 0;
 	
 
-/*
-**	Recuperer min_x et min_y pour obtenir les coordonnees de reference
-*/
-/*
-	int ligneuh;
-	int min_x;
-	int min_y;
-	int ka;
-	int iter_tetris;
+	t_map carte;
+	carte.iter_point.x = 0;
+	carte.iter_point.y = 0;
 
-	iter_tetris = 0;
-	ligneuh = 0;
-	min_y = 4;
-	min_x = 4;
-	while (iter_tetris < tetriminos_nb)
+	tetris_iter = 0;
+	while(tetris_iter < tetriminos_nb)
 	{
-		
-
-		while (ligneuh < (iter_tetris + 1) * 4)
-		{ 
-			ka = 0;
-			while (tetris_table[ligneuh][ka])
-			{
-				if (tetris_table[ligneuh][ka] == '#')
-				{
-					if (ka < min_x)
-						min_x = ka;
-					if (ligneuh < min_y)
-						min_y = ligneuh;
-				}
-				ka++;
-			}
-			ligneuh++;
-		}
-		printf("min_x %d\nmin_y %d\n", min_x, min_y);
-		ligneuh = ligneuh + 4;
-		min_x = min_x + 4;
-		min_y = min_y + 4;
-		iter_tetris++;
-	}
-*/
-
-/*
-**	Transformer les 4 premiers # en A les 4 suivants en B et ainsi de suite
-*/
-
-	/*
-		i = 0;
-		char le_char = 'A';
-		while (buf[i])
+		hash_iter = 0;
+		while(hash_iter < 4)
 		{
-			if (buf[i] == '#')
-				buf[i] = le_char;
-			if (i !=0 && (i+1) % 21 == 0)
-				le_char++;
-			i++;
+			if (map[tetrimino[tetris_iter].hash[hash_iter].y
+				- tetrimino[tetris_iter].ref_point_min.y 
+				+ carte.iter_point.y][tetrimino[tetris_iter].hash[hash_iter].x
+				- tetrimino[tetris_iter].ref_point_min.x
+				+ carte.iter_point.x] == '.')
+			{
+				printf("#%d\t", hash_iter);
+				puts("il y a de la place");
+
+				map[tetrimino[tetris_iter].hash[hash_iter].y
+					- tetrimino[tetris_iter].ref_point_min.y
+					+ carte.iter_point.y][tetrimino[tetris_iter].hash[hash_iter].x
+					- tetrimino[tetris_iter].ref_point_min.x
+					+ carte.iter_point.x] = tetrimino[tetris_iter].lettre;
+			}
+			else
+			{
+				printf("#%d\t", hash_iter);
+				puts("pas de place");
+				
+				if (carte.iter_point.y < 4)
+				{	
+					if (carte.iter_point.x < 3)
+					{
+						puts("Point deja pris");
+						carte.iter_point.x++;
+
+					}
+					if (carte.iter_point.x == 3)
+					{
+						puts("Ligne deja prise");
+						carte.iter_point.y++;
+						carte.iter_point.x = 0;
+					}
+					hash_iter = -1;
+				}
+				
+				/*
+				if (carte.iter_point.x = 3)
+				{
+					carte.iter_point.y = 
+				}
+				hash_iter = - 1;
+				*/
+			}
+			hash_iter++;
 		}
-		printf("%s\n", buf);
-	*/
+		tetris_iter++;
+	}
+
+
+	printf("\n");
+	while(*map)
+	{
+		printf("%s\n", *map);
+		map++;
+	}
+
+
 
 /*
 **	Verifier recursivement s'il est possible de poser tous les tetrimino dans le carre minimal l'augmenter juqu'a ce que ca fonctionne.

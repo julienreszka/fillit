e59c5afb29257a0fc717e733d8b9d6c51aa63125
Julien RESZKA <jreszka@e3r4p3.42.fr>
2017-01-08 13:27:54 +0100

    build tets works

--- jreszka/ft_build_tets.c
+++ jreszka/ft_build_tets.c
@@ -6,7 +6,7 @@
 /*   By: jreszka <jreszka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/06 11:06:49 by jreszka           #+#    #+#             */
-/*   Updated: 2017/01/07 14:25:56 by jreszka          ###   ########.fr       */
+/*   Updated: 2017/01/08 13:15:35 by jreszka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,6 @@ t_map ft_build_tets(char **table, t_map map, t_data data)
 {
 	while (table[++map.table_i])
 	{
-		if ((map.table_i + 1) % 4 == 0)
-		{
-			map.tets_i++;
-			map.hash_i = 0;
-		}
 		map.charac_i = -1;
 		while (table[map.table_i][++map.charac_i])
 		{
@@ -32,6 +27,28 @@ t_map ft_build_tets(char **table, t_map map, t_data data)
 				map.hash_i++;
 			}
 		}
+		if ((map.table_i + 1) % 4 == 0)
+		{
+			map.tets_i++;
+			map.hash_i = 0;
+		}
+	}
+	int i;
+	int j;
+
+	i = 0;
+	printf("-------tets------\n");
+	while (i < (data.hash_nb /4))
+	{
+		j = 0;
+		printf("------------tetris %d----------\n", i);
+		while (j < 4)
+		{
+			printf("------------hash %d----------\n", j);
+			printf("x: %d\ny: %d\n", map.tets[i].hash[j].x, map.tets[i].hash[j].y);
+			j++;
+		}
+		i++;
 	}
 	map = ft_tets_ref_points(map, data);
 	return (map);

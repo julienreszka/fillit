1ce670529de01ae4b27daf6ddbe12c0e0c7e5a41
Julien RESZKA <jreszka@e2r6p17.42.fr>
2017-01-10 22:18:37 +0100

    shows the steps, some code is commented

--- jreszka/ft_solve.c
+++ jreszka/ft_solve.c
@@ -6,7 +6,7 @@
 /*   By: jreszka <jreszka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 10:45:57 by jreszka           #+#    #+#             */
-/*   Updated: 2017/01/10 22:12:57 by jreszka          ###   ########.fr       */
+/*   Updated: 2017/01/10 22:18:03 by jreszka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,9 +75,9 @@ int		can_displace_tetri(int i, t_map map)
 		|| can_move_tet_bottom(i, map))
 
 	{
-		ft_putstr("can_displace_tetri ");
-		ft_putnbr(i);
-		ft_putstr("\n");
+		// ft_putstr("can_displace_tetri ");
+		// ft_putnbr(i);
+		// ft_putstr("\n");
 		return (1);
 	}
 	return (0);
@@ -104,9 +104,9 @@ t_map	init_tets_pos(t_map map, t_data data)
 
 t_map	rm_tet(int i, t_map map)
 {
-	ft_putstr("removing letter ");
-	ft_putchar(map.tets[i].lettre);
-	ft_putstr("\n");
+	// ft_putstr("removing letter ");
+	// ft_putchar(map.tets[i].lettre);
+	// ft_putstr("\n");
 	map.pos.y = 0;
 	while (map.pos.y < map.size)
 	{
@@ -142,7 +142,7 @@ int		is_placed_tet(int i, t_map map)
 			// ft_putstr("\n");
 			if (can_place_tet(i, map, map.tets[i].pos.x, map.tets[i].pos.y))
 			{
-				ft_putstr("Bitch placed\n");
+				// ft_putstr("Bitch placed\n");
 				place_tet(i, map, map.tets[i].pos.x, map.tets[i].pos.y);
 				return (1);
 			}
@@ -150,7 +150,7 @@ int		is_placed_tet(int i, t_map map)
 		}
 		y++;
 	}
-	ft_putstr("Bitch not placed\n");
+	// ft_putstr("Bitch not placed\n");
 	return (0);
 }
 
@@ -158,12 +158,12 @@ t_map	move_tet_pos(int i, t_map map)
 {
 	if (map.tets[i].pos.x + map.tets[i].width < map.size)
 	{
-		ft_putstr("moving pos x from ");
-		ft_putnbr(map.tets[i].pos.x);
-		ft_putstr(" to ");
+		// ft_putstr("moving pos x from ");
+		// ft_putnbr(map.tets[i].pos.x);
+		// ft_putstr(" to ");
 		map.tets[i].pos.x++;
-		ft_putnbr(map.tets[i].pos.x);
-		ft_putstr("\n");
+		// ft_putnbr(map.tets[i].pos.x);
+		// ft_putstr("\n");
 	}
 	else
 	{
@@ -176,35 +176,35 @@ t_map	move_tet_pos(int i, t_map map)
 t_map	try_place_all_tets(t_map map)
 {
 	
-	ft_putstr("is trying to place tet ");
-	ft_putnbr(map.placed_tets_nb);
-	ft_putstr("\n");
+	// ft_putstr("is trying to place tet ");
+	// ft_putnbr(map.placed_tets_nb);
+	// ft_putstr("\n");
 	if (is_placed_tet(map.placed_tets_nb, map))
 	{
 		ft_print_2d_table(map.content);
 		map.placed_tets_nb++;
 
-		ft_putstr("map.placed_tets_nb = ");
-		ft_putnbr(map.placed_tets_nb);
-		ft_putstr("\n");
-		sleep(1);
+		// ft_putstr("map.placed_tets_nb = ");
+		// ft_putnbr(map.placed_tets_nb);
+		// ft_putstr("\n");
+		// sleep(1);
 	}
 	else if (map.tets[map.placed_tets_nb].pos.y < map.size
 		&& map.tets[map.placed_tets_nb].pos.x < map.size)
 	{
-		ft_putstr("can can_displace_tetri");
+		//ft_putstr("can can_displace_tetri");
 		map = move_tet_pos(map.placed_tets_nb, map);
 	}
 	else
 	{
-		ft_putstr("OUI");
+		// ft_putstr("OUI");
 		map.tets[map.placed_tets_nb].pos.y = 0;
 		map.tets[map.placed_tets_nb].pos.x = 0;
 
 		map.placed_tets_nb--;
 		map = rm_tet(map.placed_tets_nb, map);
 		map = move_tet_pos(map.placed_tets_nb, map);
-		ft_print_2d_table(map.content);
+		// ft_print_2d_table(map.content);
 	}
 	return (map);
 }
@@ -215,9 +215,9 @@ void	ft_solve(t_map map, t_data data)
 	while (map.placed_tets_nb != (data.hash_nb / 4))
 	{
 		
-		ft_putstr("map.size = ");
-		ft_putnbr(map.size);
-		ft_putstr("\n");
+		// ft_putstr("map.size = ");
+		// ft_putnbr(map.size);
+		// ft_putstr("\n");
 		while (first_tetri_can_be_displaced(map) || map.placed_tets_nb < (data.hash_nb / 4))
 		{
 			if (map.placed_tets_nb == (data.hash_nb / 4))
@@ -226,6 +226,6 @@ void	ft_solve(t_map map, t_data data)
 		}
 		map.size++;
 	}
-	ft_putstr("FINAL\n");
+	ft_putstr("FINAL\n\n");
 	ft_print_2d_table(map.content);
 }

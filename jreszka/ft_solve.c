35dda13009c4f5c1a1c04357477d7523492b0793
Julien RESZKA <jreszka@e2r5p19.42.fr>
2017-01-11 19:05:13 +0100

    factorised solve

--- jreszka/ft_solve.c
+++ jreszka/ft_solve.c
@@ -6,113 +6,15 @@
 /*   By: jreszka <jreszka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 10:45:57 by jreszka           #+#    #+#             */
-/*   Updated: 2017/01/11 18:30:54 by jreszka          ###   ########.fr       */
+/*   Updated: 2017/01/11 19:04:04 by jreszka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		can_place_tet(int i, t_map map, int pos_x, int pos_y)
-{
-	int available_places;
-	int j;
-	int x;
-	int y;
-
-	available_places = 0;
-	j = 0;
-	while (j < 4)
-	{
-		y = pos_y + map.tets[i].hash[j].y - map.tets[i].ref_min.y;
-		x = pos_x + map.tets[i].hash[j].x - map.tets[i].ref_min.x;
-		if (x < map.size && y < map.size)
-		{
-			if (map.content[y][x] == '.')
-				available_places++;
-		}
-		j++;
-	}
-	if (available_places == 4)
-		return (1);
-	return (0);
-}
-
-void	place_tet(int i, t_map map, int pos_x, int pos_y)
-{
-	int j;
-	int x;
-	int y;
-
-	j = 0;
-	while (j < 4)
-	{
-		y = pos_y + map.tets[i].hash[j].y - map.tets[i].ref_min.y;
-		x = pos_x + map.tets[i].hash[j].x - map.tets[i].ref_min.x;
-		map.content[y][x] = map.tets[i].lettre;
-		j++;
-	}
-}
-
-int		can_move_tet_right(int i, t_map map)
-{
-	if (map.tets[i].width + map.tets[i].pos.x < map.size)
-		return (1);
-	return (0);
-}
-
-int		can_move_tet_bottom(int i, t_map map)
-{
-	if (map.tets[i].height + map.tets[i].pos.y < map.size)
-	{
-		return (1);
-	}
-	return (0);
-}
-
-int		can_displace_tetri(int i, t_map map)
-{
-	if (can_move_tet_right(i, map)
-		|| can_move_tet_bottom(i, map))
-	{
-		return (1);
-	}
-	return (0);
-}
-
 int		first_tetri_can_be_displaced(t_map map)
 {
 	return (can_displace_tetri(0, map));
-}
-
-t_map	init_tets_pos(t_map map, t_data data)
-{
-	int i;
-
-	i = 0;
-	while (i < (data.hash_nb / 4))
-	{
-		map.tets[i].pos.x = 0;
-		map.tets[i].pos.y = 0;
-		i++;
-	}
-	return (map);
-}
-
-t_map	rm_tet(int i, t_map map)
-{
-	map.pos.y = 0;
-	while (map.pos.y < map.size)
-	{
-		map.pos.x = 0;
-		while (map.pos.x < map.size)
-		{
-			if (map.content[map.pos.y][map.pos.x] == map.tets[i].lettre)
-				map.content[map.pos.y][map.pos.x] = '.';
-			map.pos.x++;
-		}
-		map.pos.y++;
-	}
-	return (map);
 }
 
 int		is_placed_tet(int i, t_map map)
@@ -138,16 +40,16 @@ int		is_placed_tet(int i, t_map map)
 	return (0);
 }
 
-t_map	move_tet_pos(int i, t_map map)
+t_map	init_tets_pos(t_map map, t_data data)
 {
-	if (map.tets[i].pos.x + map.tets[i].width < map.size)
-	{
-		map.tets[i].pos.x++;
-	}
-	else
+	int i;
+
+	i = 0;
+	while (i < (data.hash_nb / 4))
 	{
 		map.tets[i].pos.x = 0;
-		map.tets[i].pos.y++;
+		map.tets[i].pos.y = 0;
+		i++;
 	}
 	return (map);
 }
